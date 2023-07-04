b371eb691790b21cad6bb88331e2bb5e56758622
Kareem Ebrahem <90919499+mrx7014@users.noreply.github.com>
2023-07-04 16:16:46 +0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -8,7 +8,4 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/omni_a23.mk
 
-COMMON_LUNCH_CHOICES := \
-    omni_a23-user \
-    omni_a23-userdebug \
-    omni_a23-eng
+COMMON_LUNCH_CHOICES += omni_a23-eng
