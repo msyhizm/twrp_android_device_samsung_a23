46b1951561fa6b63906d5f1de1e6369383f3850a
Kareem Ebrahem <90919499+mrx7014@users.noreply.github.com>
2023-07-08 14:02:27 +0300

    Update and rename omni_a23.mk to twrp_a23.mk

--- twrp_a23.mk
+++ twrp_a23.mk
@@ -16,7 +16,7 @@ $(call inherit-product, vendor/omni/config/common.mk)
 $(call inherit-product, device/samsung/a23/device.mk)
 
 PRODUCT_DEVICE := a23
-PRODUCT_NAME := omni_a23
+PRODUCT_NAME := twrp_a23
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-A235F
 PRODUCT_MANUFACTURER := samsung
